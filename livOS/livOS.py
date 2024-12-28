be0970da69417dc790426cc42474cf75f8834a59
livius09 <levi.wimroither@hotmail.com>
2024-12-28 15:05:20 +0100

    added exploit_suite and creds are read from files

--- livOS/livOS.py
+++ livOS/livOS.py
@@ -5,8 +5,23 @@ import hashlib
 host= '127.0.0.1'
 port=12345
 
-user=["levi","ligma"]
-psw= [hashlib.sha256("balls".encode()),hashlib.sha256("balls".encode())]
+with open("userf.txt", "r") as file:  # "r" is for read mode
+    content = file.read()
+    users=content.split(",")
+
+
+with open("pswf.txt", "r") as file:  # "r" is for read mode
+    content = file.read()
+    paswords=content.split(",")
+
+
+with open("authf.txt", "r") as file:  # "r" is for read mode
+    content = file.read()
+    auths=content.split(",")
+
+
+curuser=None
+curauth=None
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -30,7 +45,7 @@ try:
             response="try help for a list of comands"
 
             print("dat:"+dat)
-            if dat == "Help":
+            if dat == "help":
                 response =(
                 "---------- \n"
                 "This is a WIP goffi ah server \n" 
@@ -38,7 +53,8 @@ try:
                 "comands: \n" "-Help \n"
                 "-kill \n"
                 "-echo \n"
-                "-exe")
+                "-exe \n"
+                "-login")
 
             elif dat == "kill":
                 client_sok.sendall("Goodbey from livOS".encode())
@@ -47,6 +63,23 @@ try:
             elif dat == "echo":
                 client_sok.sendall(client_sok.recv(1024))
                 continue
+
+            elif dat == "login":
+                client_sok.sendall("Username: ".encode())
+                username = client_sok.recv(1024).decode().strip()
+                client_sok.sendall("Pasword: ".encode())
+                pasword = client_sok.recv(1024).decode().strip()
+                if username in users and hashlib.sha256(pasword.encode()).hexdigest()==paswords[users.index(username)]:
+                    curuser=username
+                    curauth=auths[users.index(curuser)]
+                    response=f"login sucsesfull welcome {curuser} whit auth level {curauth} from ip: {client_adr}"
+                else:
+                    response="login faild wrong usser name or pasword"
+                
+
+
+
+
             
             response=response+"\n"
             client_sok.sendall(response.encode())
