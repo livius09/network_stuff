face55c652cdb571de2f1ecbca1ac69c760988b8
livius09 <levi.wimroither@hotmail.com>
2026-09-25 11:50:37 +0200

    added db support and some comands

--- QuarzChat/QuarzChat.py
+++ QuarzChat/QuarzChat.py
@@ -18,6 +18,22 @@ except (OSError, j.JSONDecodeError) as e:
     raise SystemExit(f"couldn't load config: {e}")
 
 
+conn = sqlite3.connect("QuarzChat/chat.db", check_same_thread=False)
+conn.execute("""CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user TEXT NOT NULL, 
+    text TEXT NOT NULL,
+    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
+
+conn.execute("""CREATE TABLE IF NOT EXISTS stats (
+    username        TEXT PRIMARY KEY,
+    messages_sent   INTEGER DEFAULT 0,
+    comands_used    INTEGER DEFAULT 0,
+    last_seen       TIMESTAMP)""")
+
+conn.commit()
+
+
 def socket_to_name(sock:socket.socket)->str|None:
     with lock:
         for n in clients.keys():
@@ -27,19 +43,28 @@ def socket_to_name(sock:socket.socket)->str|None:
         return None
 
 
-def broadcast(msg:str,sendsock:socket.socket|None=None):
+def broadcast(msg:str, user:str="", sendsock:socket.socket|None=None):
     with lock:
         dead = []
+
+        if user!="":
+            msg = user +": "+ msg
+
         print(msg)
+
+        conn.execute("INSERT INTO messages (user, text) VALUES (?,?)",
+                                        (user, msg))
+        conn.commit()
+
         for s in clients.keys():
             if clients[s] is not sendsock:
                 try:
                     clients[s].sendall((msg+"\n").encode())
+                    
                 except: dead.append(s)
 
         for d in dead:
             del clients[d]
-
 
 
 def handle(sock:socket.socket):
@@ -65,20 +90,64 @@ def handle(sock:socket.socket):
             return
         
         clients[user] = sock
-        
 
-    broadcast(f"*** {user} joined",sock)
+
+    with lock:
+
+        conn.execute("""
+                        INSERT INTO stats (username, messages_sent, comands_used, last_seen)
+                        VALUES (?, 1, 0, CURRENT_TIMESTAMP)
+                        ON CONFLICT(username) DO UPDATE SET
+                        last_seen = CURRENT_TIMESTAMP
+                        """, (user,))
+        conn.commit()
+        rows = conn.execute(
+            "SELECT user, text FROM messages ORDER BY id DESC LIMIT 20"
+        ).fetchall()
+
+    for u, t in reversed(rows):
+        try:
+            tren=": "
+            if u=="":
+                tren=""
+
+            sock.sendall(f"{u}{tren}{t}\n".encode())
+        except:
+            try:
+                sock.close()
+            except: pass
+            return
+
+    broadcast(f"{user} joined","***",sock)
 
     try:
         for line in f:
             line = line.strip()
 
-            broadcast(f"{user}: {line}",sock)
+            broadcast(f"{user}: {line}", "", sock)
+
+            with lock:
+                conn.execute("""
+                                INSERT INTO stats (username, messages_sent, comands_used, last_seen)
+                                VALUES (?, 1, 0, CURRENT_TIMESTAMP)
+                                ON CONFLICT(username) DO UPDATE SET
+                                messages_sent = messages_sent + 1,
+                                last_seen = CURRENT_TIMESTAMP
+                                """, (user,))
+                conn.commit()
 
             if line.startswith("/"):
+                with lock:
+                    conn.execute("""UPDATE stats set comands_used = comands_used + 1 WHERE username = ?;""",(user,))
+                    conn.commit()
+                    
+
+                
                 handleComands(line,sock)
+                
 
             if sock not in clients.values():
+                sock.close()
                 return
     finally:
         try:
@@ -90,22 +159,54 @@ def handle(sock:socket.socket):
 
 
 def handleComands(msg:str,socke:socket.socket):
-    comand = msg.split(" ")[0]
-    match comand:
+    comand = msg.split(" ")
+    match comand[0]:
         case "/help":
-            broadcast("*** ")
+            broadcast("*** here is help")
 
         case "/time":
-            broadcast("*** Time: "+ str(time.localtime()))
+            lt=time.localtime()
+            broadcast(f"Time: {lt.tm_hour}:{lt.tm_min}:{lt.tm_sec}  {lt.tm_mday},{lt.tm_mon},{lt.tm_year}","***")
 
         case "/info":
-            broadcast(f"*** IP: {setings["ip"]}\nPort: {setings["port"]}\nUptime: {time.time()-start_time}")
+            broadcast(f" IP: {setings["ip"]}\nPort: {setings["port"]}\nUptime: {(time.time()-start_time):.2f}", "***")
 
         case "/nusers":
             with lock:
                 tmp = len(clients)
 
-            broadcast(f"*** {tmp} users conected")
+            broadcast(f"{tmp} users conected","***")
+
+        case "/whois":
+            if len(comand)>1:
+                usern = comand[1]
+                print("what")
+                with lock:
+                    if usern in clients.keys():
+                        print("skib")
+                        try:
+                            broadcast(f"{usern}: {clients[usern].getpeername()[0]} {clients[usern].getpeername()[1]}","***")
+                        except Exception as e:
+                            print(e)
+                        print("elo")
+                    else:
+                        broadcast("user not found","***")
+            else:
+                broadcast("(/whois) needs argument")
+
+
+
+        case "/stats":
+            if len(comand)>1:
+                usern = comand[1]
+                with lock:
+                    stats = conn.execute("SELECT messages_sent, comands_used  WHERE username = ?",(usern,)).fetchall()
+                    if stats is not None:
+                        broadcast(f"{usern}: mesages: {stats[0]} cmds: {stats[1]}","***")
+                    else:
+                        broadcast("user not found","***")
+            else:
+                broadcast("(/stats) needs argument","***")
 
         case "/end":
             tmp = socket_to_name(socke)
@@ -114,6 +215,9 @@ def handleComands(msg:str,socke:socket.socket):
                 if tmp is not None:
                     del clients[tmp]
             socke.close()
+
+        case _:
+            broadcast(f"comand {comand[0]} not found\ntry /help")
 
 
 
@@ -124,7 +228,9 @@ srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 srv.bind((setings["ip"], setings["port"]))
 srv.listen(setings["maxConections"])
 srv.settimeout(3)
-print("server started")
+
+broadcast("server started","***")
+print(f"ncat {setings["ip"]} {setings["port"]}")
 
 try:
     while True:
@@ -135,8 +241,8 @@ try:
             pass
 except KeyboardInterrupt:
     pass
+except Exception as e:
+    print(e)
 finally:
     broadcast("*** server Shuting down")
     srv.close()
-
-
